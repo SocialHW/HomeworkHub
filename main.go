8ca47869fc6f63211dcb0da9d705906ca156e566
Robby Zambito <Zambito101@gmail.com>
2018-10-10 15:30:11 -0400

    Created basic templating

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("static/")))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	})
 
 	log.Println("Server running...")
 	http.ListenAndServe(":3000", nil)
