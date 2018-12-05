83741500f16bc4d25a09305df8454def5a589843
Nathaniel Mitts <njmitts@gmail.com>
2018-12-05 17:33:17 -0500

    changed statement names

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ func main() {
 }
 func initialize_DB() {
 	database, _ = sql.Open("sqlite3", "./homeworkHub.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS userInfo (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)")
-	statement.Exec()
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS post_info (post_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, file_path TEXT)")
-	statement.Exec()
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS comment_section (post_id INTEGER, username TEXT, comment TEXT)")
-	statement.Exec()
+	statement1, _ := database.Prepare("CREATE TABLE IF NOT EXISTS userInfo (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)")
+	statement1.Exec()
+	statement2, _ := database.Prepare("CREATE TABLE IF NOT EXISTS post_info (post_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, file_path TEXT)")
+	statement2.Exec()
+	statement3, _ := database.Prepare("CREATE TABLE IF NOT EXISTS comment_section (post_id INTEGER, username TEXT, comment TEXT)")
+	statement3.Exec()
 }
